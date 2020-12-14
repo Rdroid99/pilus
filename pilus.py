a9a984c313d1380c9482f54d8bbd35c17615c874
Rahmat Hidayat <74264262+Rdroid99@users.noreply.github.com>
2020-12-14 13:24:30 +0700

    Add files via upload

---
+++ pilus.py
@@ -0,0 +1,21 @@
+#coder by Rdroid99
+# date : 12:30
+# Recode tidak akan membuatmu jadi programer
+#Looping with oray python
+# LOLI HUNTER
+# Pilus garudanya kaka
+#GABUT:V
+import os,sys
+from time import sleep
+os.system("clear")
+os.system("toilet -F gay Pilus")
+print("\033[32m Ketik Ctrl + Z untuk menghentikan")
+#looping
+os.system("date")
+target =  input("\033[35mMasukan no targetnya Ajg:\033[31m ")
+print("\033[00m")
+sleep(1)
+while(target):
+  print("\033[34mMengirim Pilus ke nomer\033[31m",target)
+#keluar
+print("\033[31m Masukan yg bener ajg\033[00m")
